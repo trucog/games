0df5454d1c7140b919221cccaa1238c90ea8404c
trucog <61084092+trucog@users.noreply.github.com>
2020-02-27 06:59:02 -0800

    Update S3.tf
    Creating S3 Bucket in us-east1

--- Terraform/S3.tf
+++ Terraform/S3.tf
@@ -1,1 +1,12 @@
 
+
+provider "aws" {
+  access_key = "*****************"
+  secret_key = "***************"
+   region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucketven1"
+
+}
