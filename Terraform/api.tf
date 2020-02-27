95b4360d5cfec046b331d1c75004cb7350edba94
trucog <61084092+trucog@users.noreply.github.com>
2020-02-27 07:51:57 -0800

    Update api.tf
    Code for User Management Resources and Methods

--- Terraform/api.tf
+++ Terraform/api.tf
@@ -1,1 +1,19 @@
 
+
+resource "aws_api_gateway_rest_api" "UserManagementAPI" {
+  name        = "UserManagementAPI"
+  description = "This is my API for demonstrating the User Management"
+}
+
+resource "aws_api_gateway_resource" "UserManagementResource" {
+  rest_api_id = "${aws_api_gateway_rest_api.UserManagementAPI.id}"
+  parent_id   = "${aws_api_gateway_rest_api.UserManagementAPI.root_resource_id}"
+  path_part   = "usermanagementresource"
+}
+
+resource "aws_api_gateway_method" "UserManagementMethod" {
+  rest_api_id   = "${aws_api_gateway_rest_api.UserManagementAPI.id}"
+  resource_id   = "${aws_api_gateway_resource.usermanagementresource.id}"
+  http_method   = "GET"
+  authorization = "NONE"
+}
